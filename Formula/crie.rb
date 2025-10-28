43e0efc28423dcdaf0a0edb776f8e051408e3ba5
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-10-28 19:23:16 +0000

    Add crie formula

---
+++ Formula/crie.rb
@@ -0,0 +1,45 @@
+class Crie < Formula
+  desc "Universal meta-linter using containerized execution"
+  homepage "https://github.com/tyhal/crie"
+  url "https://github.com/tyhal/crie/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ba36e32d7b372977576b26d47c0d6870ee57654f6ba66777df97b759ba52b6e3"
+  license "MIT"
+
+  head "https://github.com/tyhal/crie.git", branch: "main"
+
+  depends_on "gnupg" => :build
+  depends_on "go" => :build
+  depends_on "gpgme" => :build
+  depends_on "libassuan" => :build
+  depends_on "libgpg-error" => :build
+  depends_on "pkg-config" => :build
+
+  on_linux do
+    depends_on "llvm" => :build
+  end
+
+  def install
+    if OS.linux?
+      ENV["CC"] = Formula["llvm"].opt_bin/"clang"
+      ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
+      ENV.prepend_path "PATH", Formula["llvm"].opt_bin
+
+      # Make sure pkg-config finds gpgme and its dependencies
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["gpgme"].opt_lib/"pkgconfig"
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["libgpg-error"].opt_lib/"pkgconfig"
+      ENV.prepend_path "PKG_CONFIG_PATH", Formula["libassuan"].opt_lib/"pkgconfig"
+    end
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/crie"
+
+    generate_completions_from_executable(bin/"crie", "completion")
+  end
+
+  test do
+    assert_match "crie version #{version}", shell_output("#{bin}/crie --version")
+  end
+end
