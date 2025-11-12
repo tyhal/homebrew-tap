644051e20fe25a7927f5fa483a07d3f8b3e5a8bc
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-11-12 01:49:30 +0000

    crie: update 1.0.4 bottle.

--- Formula/crie.rb
+++ Formula/crie.rb
@@ -8,9 +8,9 @@ class Crie < Formula
   head "https://github.com/tyhal/crie.git", branch: "main"
 
   bottle do
-    root_url "https://ghcr.io/v2/tyhal/homebrew-tap"
-    sha256 cellar: :any,                 arm64_sequoia: "aa5dbb8040945f2e89147b053fd7448977390e059d2d9d4a39ce7a8878ff7c6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b83678f153a77c63e8e02be02b51e27cbcfafd729fa6739ccb5134b8d70d146b"
+    root_url "https://github.com/tyhal/homebrew-tap/releases/download/crie-1.0.4"
+    sha256 cellar: :any,                 arm64_sequoia: "84a1a20d81f005c85f8a7b0d1b78ca45a51c7976c7f18fd9563a82c6574bb4fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ad8a669b97bbe691bbcca6e7d01c47f1958d71eac97e970b4068ab6d85599bd"
   end
 
   depends_on "gnupg" => :build
