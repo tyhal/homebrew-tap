2877a05ae020f1704f024a97cd674e2a8bc2e5c2
Tyler Hale <11653981+tyhal@users.noreply.github.com>
2025-12-26 01:26:00 +0000

    crie with no cgo

--- Formula/crie.rb
+++ Formula/crie.rb
@@ -13,43 +13,16 @@ class Crie < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e081671fbdf8ae5d958cc2916e91703d72b8f5a724195d2e403087fdb53ee6d"
   end
 
-  depends_on "gnupg" => :build
   depends_on "go" => :build
-  depends_on "pkg-config" => :build
-
-  if OS.linux?
-    depends_on "libgpg-error"
-    depends_on "libassuan"
-
-    depends_on "llvm" => :build
-    depends_on "btrfs-progs" => :build
-  end
-
-  depends_on "gpgme"
 
   def install
-    if OS.linux?
-      ENV["CC"] = Formula["llvm"].opt_bin/"clang"
-      ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
-      ENV.prepend_path "PATH", Formula["llvm"].opt_bin
-
-      # Pkg Config
-      Formula["crie"].recursive_dependencies.each do |dep|
-        f = dep.to_formula
-
-        # pkg-config directories
-        pc = f.opt_lib/"pkgconfig"
-        ENV.prepend_path "PKG_CONFIG_PATH", pc if pc.directory?
-
-        # include directories
-        inc = f.opt_include
-        ENV.prepend_path "C_INCLUDE_PATH", inc if inc.directory?
-      end
-    end
     ldflags = %W[
       -s -w
       -X main.version=#{version}
     ]
+
+    ENV["GOFLAGS"] = '-tags="exclude_graphdriver_btrfs containers_image_openpgp"'
+    ENV["CGO_ENABLED"] = "0"
 
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/crie"
 
